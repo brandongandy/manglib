9b8800f1c059da26b4c6c61c5bde73a2e35108fe
Brandon Gandy <tallain@gmail.com>
2022-04-29 21:34:40 -0700

    tidy up name generator and fix infinite loop

--- manglib/PseudoWordGenerator.cs
+++ manglib/PseudoWordGenerator.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using Mang.Utils;
 
 namespace Mang
 {
@@ -18,11 +19,10 @@ namespace Mang
     private static readonly TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
 
     private readonly List<string> starters = new List<string>();
+    private readonly HashSet<string> enders = new HashSet<string>();
 
-    private readonly Dictionary<char, List<string>> gramDict =
-        Enumerable
-            .Range('a', 'z')
-            .ToDictionary(a => (char)a, _ => new List<string>());
+    private readonly Dictionary<char, List<string>> gramDict = 
+      new Dictionary<char, List<string>>();
 
     #endregion
 
@@ -30,17 +30,17 @@ namespace Mang
 
     public PseudoWordGenerator(IEnumerable<string> words, int gramLen)
     {
-      foreach (var word in words.Select(w => w.Trim().ToLower()).Where(w => w.Length > gramLen)
-          .Where(w => Regex.IsMatch(w, "^[a-z]+$")))
+      foreach (var word in words.Select(w => w.Trim().ToLower()).Where(w => w.Length > gramLen))
       {
         starters.Add(word[..gramLen]);
+        enders.Add(word.Substring(word.Length - gramLen, gramLen));
         for (var i = 0; i < word.Length - gramLen; i++)
         {
           var currentLetter = word[i];
           if (!gramDict.TryGetValue(currentLetter, out var grams))
           {
-            i = word.Length;
-            continue;
+            grams = new List<string>();
+            gramDict.Add(currentLetter, grams);
           }
 
           grams.Add(word.Substring(i + 1, gramLen));
@@ -55,19 +55,35 @@ namespace Mang
     public string GenerateWord(int wordLength)
     {
       var result = new StringBuilder(GetRandomStarter());
+      var lastGram = string.Empty;
+      var numberOfTries = 0;
 
-      while (result.Length < wordLength)
+      while ((result.Length < wordLength || enders.Contains(lastGram)) &&
+        numberOfTries < 100)
       {
         try
         {
+          lastGram = GetRandomGram(result[^1]);
           result.Append(GetRandomGram(result[^1]));
         }
         catch
         {
           // If the gram has no following characters,
           // restart the randomization with a new starter.
-          result.Append(GetRandomStarter());
+          lastGram = GetRandomStarter();
+          result.Append(lastGram);
         }
+        numberOfTries++;
+      }
+
+      while (result[^2] == ' ' || result[^2] == '-')
+      {
+        result = result.Remove(result.Length - 2, 2);
+      }
+
+      while (result[^1] == '-')
+      {
+        result = result.Remove(result.Length - 1, 1);
       }
 
       return textInfo.ToTitleCase(result.ToString());
--- manglib/Utils/StringExtensions.cs
+++ manglib/Utils/StringExtensions.cs
@@ -81,6 +81,23 @@ namespace Mang.Utils
       return true;
     }
 
+    public static string RemoveDiacritics(this string text)
+    {
+      var normalizedString = text.Normalize(NormalizationForm.FormD);
+      var stringBuilder = new StringBuilder();
+
+      foreach (var c in normalizedString.EnumerateRunes())
+      {
+        var unicodeCategory = Rune.GetUnicodeCategory(c);
+        if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+        {
+          stringBuilder.Append(c);
+        }
+      }
+
+      return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+    }
+
     /// <summary>
     /// Determines whether the given character is a vowel. Treats all non-letter characters as consonants.
     /// </summary>
