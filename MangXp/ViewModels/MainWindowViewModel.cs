43cd14bff642b10112f8e8b8d102c200267cf1f3
Brandon Gandy <tallain@gmail.com>
2023-12-01 16:51:04 -0800

    fixed some spacing issues, cleaned up some code

--- MangXp/ViewModels/MainWindowViewModel.cs
+++ MangXp/ViewModels/MainWindowViewModel.cs
@@ -10,46 +10,8 @@ namespace MangXp.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-  private ViewModelBase contentViewModel;
-
-  public MangViewModel MangView { get; set; }
-
-  public ViewModelBase ContentViewModel
-  {
-    get => contentViewModel;
-    private set => this.RaiseAndSetIfChanged(ref contentViewModel, value);
-    
-  }
-
   public MainWindowViewModel()
   {
-    //MangView = new MangViewModel();
-    //contentViewModel = MangView;
   }
-
-  public void SourceCombo_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
-  {
-    Console.WriteLine("foo");
-  }
-
-  //public void AddItem()
-  //{
-  //  AddItemViewModel addItemViewModel = new();
-
-  //  _ = Observable.Merge(
-  //      addItemViewModel.OkCommand,
-  //      addItemViewModel.CancelCommand.Select(_ => (ToDoItem?)null))
-  //    .Take(1)
-  //    .Subscribe(newItem =>
-  //    {
-  //      if (newItem != null)
-  //      {
-  //        ToDoList.ListItems.Add(newItem);
-  //      }
-  //      ContentViewModel = ToDoList;
-  //    });
-
-  //  ContentViewModel = addItemViewModel;
-  //}
 }
 
